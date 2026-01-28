90233cdf49468cae41a84674dea49907a9b22482
Matchab78 <mathischab78@gmail.com>
2026-01-28 16:50:48 +0100

    supabase update

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./utils/authContext";
-
 import StackNavigator from "./navigation/StackNavigator";
 
 export default function App() {
