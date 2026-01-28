7a5db26d3dd14efedd5689ebf8ac77e9ff7f87ef
QuentinOTT <159418139+QuentinOTT@users.noreply.github.com>
2026-01-28 19:28:26 +0100

    fix

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { AuthProvider } from "./utils/authContext";
-import StackNavigator from "./navigation/StackNavigator";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './utils/authContext';
+import StackNavigator from './navigation/StackNavigator';
 
 export default function App() {
   return (
